48c5d54e496d51131508f8737ecf31a47c71f3a9
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-10-29 01:28:21 +0100

    Add tests for Delete if exists

--- Wororo.Utilities.UnitTests/FileExtensionsTests.cs
+++ Wororo.Utilities.UnitTests/FileExtensionsTests.cs
@@ -48,5 +48,44 @@ namespace Wororo.Utilities.UnitTests
             Assert.IsNotEmpty(readLines);
             Assert.AreEqual(10, readLines.Count());
         }
+
+        [Test]
+        public void TestDeleteIfExistsDirectory()
+        {
+            var path = "tempFolder";
+
+            path.DeleteIfExists();
+            Assert.False(Directory.Exists(path));
+
+            Directory.CreateDirectory(path);
+            Assert.True(Directory.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(Directory.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(Directory.Exists(path));
+        }
+
+        [Test]
+        public void TestDeleteIfExistsFile()
+        {
+            var path = "tempFile";
+
+            path.DeleteIfExists();
+            Assert.False(File.Exists(path));
+
+            using (var file = File.Create(path))
+            {
+            }
+
+            Assert.True(File.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(File.Exists(path));
+
+            path.DeleteIfExists();
+            Assert.False(File.Exists(path));
+        }
     }
 }
