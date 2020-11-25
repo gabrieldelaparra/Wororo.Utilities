bcfccbd3957011bb23fb2e4202e290daa98620d2
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-11-25 08:45:10 +0100

    Commenting out CsvSerialization until it is properly designed;

--- Wororo.Utilities/CsvSerialization.cs
+++ Wororo.Utilities/CsvSerialization.cs
@@ -5,32 +5,32 @@ using System.Text;
 
 namespace Wororo.Utilities
 {
-    public static class CsvSerialization
-    {
-        public static T DeserializeCsv<T>(string inputJsonFilename)
-        {
-            if (inputJsonFilename.IsEmpty())
-                return default;
-            if (!File.Exists(inputJsonFilename))
-                return default;
-            //var text = File.ReadAllText(inputJsonFilename);
-            //return JsonConvert.DeserializeObject<T>(text);
-            return default;
-        }
+    //public static class CsvSerialization
+    //{
+    //    public static T DeserializeCsv<T>(string inputJsonFilename)
+    //    {
+    //        if (inputJsonFilename.IsEmpty())
+    //            return default;
+    //        if (!File.Exists(inputJsonFilename))
+    //            return default;
+    //        //var text = File.ReadAllText(inputJsonFilename);
+    //        //return JsonConvert.DeserializeObject<T>(text);
+    //        return default;
+    //    }
 
-        public static void SerializeCsv(this object objectToSerialize, string outputJsonFilename)
-        {
-            if (outputJsonFilename.IsEmpty())
-                return;
+    //    public static void SerializeCsv(this object objectToSerialize, string outputJsonFilename)
+    //    {
+    //        if (outputJsonFilename.IsEmpty())
+    //            return;
 
-            outputJsonFilename.CreatePathIfNotExists();
+    //        outputJsonFilename.CreatePathIfNotExists();
 
-            //var serializer = new JsonSerializer
-            //        { NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented };
+    //        //var serializer = new JsonSerializer
+    //        //        { NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented };
 
-            //using var sw = new StreamWriter(outputJsonFilename);
-            //using JsonWriter writer = new JsonTextWriter(sw);
-            //serializer.Serialize(writer, objectToSerialize);
-        }
-    }
+    //        //using var sw = new StreamWriter(outputJsonFilename);
+    //        //using JsonWriter writer = new JsonTextWriter(sw);
+    //        //serializer.Serialize(writer, objectToSerialize);
+    //    }
+    //}
 }
