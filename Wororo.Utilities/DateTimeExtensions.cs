9c75c0efdeb21ae253e5398ec66b7b8c10d32168
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-11-25 08:44:13 +0100

    Add extensions for datetime (StartOfWeek and CalendarWeek)

---
+++ Wororo.Utilities/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Wororo.Utilities
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime StartOfWeek(this DateTime date, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        {
+            var diff = (7 + (date.DayOfWeek - startOfWeek)) % 7;
+            return date.AddDays(-1 * diff).Date;
+        }
+
+        public static int GetCalendarWeek(this DateTime date) => CultureInfo.CurrentCulture.Calendar
+            .GetWeekOfYear(date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
+    }
+}
