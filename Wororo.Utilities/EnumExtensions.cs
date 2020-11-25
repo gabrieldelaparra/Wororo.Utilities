37902708f140ddd7cb819f2c85ef465d70037237
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-11-25 08:43:44 +0100

    Add EnumExtensions (Cycle throught them)

---
+++ Wororo.Utilities/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Wororo.Utilities
+{
+    public static class EnumExtensions
+    {
+        public static T Cycle<T>(this T value) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum) return value;
+
+            var values = (T[])Enum.GetValues(value.GetType());
+            var length = values.Length;
+            if (length < 2) 
+                return value;
+
+            var intValue = Array.IndexOf(values, value);
+            return values[++intValue % length];
+        }
+    }
+}
