ef49145f71ce99dbf0d7845148c3ad313105ee25
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2021-04-13 11:09:55 +0200

    Json Serializer now includes the TypeNameHandling if requried (Auto). useful for interfaces/covariance;

--- Wororo.Utilities/JsonSerialization.cs
+++ Wororo.Utilities/JsonSerialization.cs
@@ -1,21 +1,29 @@
 ﻿using System.IO;
-
 using Newtonsoft.Json;
 
 namespace Wororo.Utilities
 {
     public static class JsonSerialization
     {
-        public static T DeserializeJson<T>(string inputJsonFilename) {
+        public static T DeserializeJson<T>(string inputJsonFilename)
+        {
             if (inputJsonFilename.IsEmpty())
                 return default;
             if (!File.Exists(inputJsonFilename))
                 return default;
-            var text = File.ReadAllText(inputJsonFilename);
-            return JsonConvert.DeserializeObject<T>(text);
+            var serializer = new JsonSerializer
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.Auto,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            using var sw = new StreamReader(inputJsonFilename);
+            using JsonReader writer = new JsonTextReader(sw);
+            return serializer.Deserialize<T>(writer);
         }
 
-        public static void SerializeJson(this object objectToSerialize, string outputJsonFilename) {
+        public static void SerializeJson(this object objectToSerialize, string outputJsonFilename)
+        {
             if (outputJsonFilename.IsEmpty())
                 return;
 
@@ -23,8 +31,10 @@ namespace Wororo.Utilities
             var serializer = new JsonSerializer
             {
                 NullValueHandling = NullValueHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.Auto,
                 Formatting = Formatting.Indented,
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
             };
 
             using var sw = new StreamWriter(outputJsonFilename);
