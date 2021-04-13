85eaa0c30230fa63b9b7b6596004baaa226e7417
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2021-04-13 11:08:47 +0200

    Implement AppendSafe
    Adds text to an existing file, creates it if does not exists. Deals with write permission in a brute force way.

--- Wororo.Utilities/FileExtensions.cs
+++ Wororo.Utilities/FileExtensions.cs
@@ -3,50 +3,91 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Wororo.Utilities
 {
     public static class FileExtensions
     {
-        public static string CleanFileName(this string fileName) {
+        public static void AppendSafe(string filename, string line)
+        {
+            AppendSafe(filename, new[] {line});
+        }
+
+        public static void AppendSafe(string filename, IEnumerable<string> lines)
+        {
+            if (!File.Exists(filename))
+            {
+                filename.CreatePathIfNotExists();
+                File.WriteAllLines(filename, lines);
+            }
+
+            try
+            {
+                File.AppendAllLines(filename, lines);
+            }
+            catch
+            {
+                Thread.Sleep(100);
+                try
+                {
+                    File.AppendAllLines(filename, lines);
+                }
+                catch
+                {
+                    Thread.Sleep(100);
+                    File.AppendAllLines(filename, lines);
+                }
+            }
+        }
+
+        public static string CleanFileName(this string fileName)
+        {
             return Regex.Replace(fileName, "[^a-zA-Z0-9_.]+", string.Empty, RegexOptions.Compiled);
         }
 
-        public static void CreatePathIfNotExists(this string filepath) {
+        public static void CreatePathIfNotExists(this string filepath)
+        {
             var fileInfo = new FileInfo(filepath);
             if (!fileInfo.Directory.Exists)
                 fileInfo.Directory.Create();
         }
 
-        public static void DeleteIfExists(this string path, bool additionalCondition = true) {
+        public static void DeleteIfExists(this string path, bool additionalCondition = true)
+        {
             if (Directory.Exists(path) && additionalCondition) Directory.Delete(path, true);
 
             if (File.Exists(path) && additionalCondition) File.Delete(path);
         }
 
-        public static IEnumerable<string> ReadLines(this Stream fileStream) {
+        public static IEnumerable<string> ReadLines(this Stream fileStream)
+        {
             using var streamReader = new StreamReader(fileStream);
             while (!streamReader.EndOfStream)
                 yield return streamReader.ReadLine();
         }
 
-        public static IEnumerable<string> ReadLines(this string filename) {
+        public static IEnumerable<string> ReadLines(this string filename)
+        {
             using var fileStream = File.OpenRead(filename);
             foreach (var line in ReadLines(fileStream))
                 yield return line;
         }
 
         public static void RestoreTemplateFileIfDoesNotExists(string assemblyFullName, string templateName,
-                string outputFilename, bool overwrite = false) {
+            string outputFilename, bool overwrite = false)
+        {
             var f = new FileInfo(outputFilename);
             if (f.Directory != null && !f.Directory.Exists) f.Directory.Create();
 
-            if (!File.Exists(outputFilename) || overwrite) {
+            if (!File.Exists(outputFilename) || overwrite)
+            {
                 var assemblyList = AppDomain.CurrentDomain.GetAssemblies();
 
                 var assembly = assemblyList.FirstOrDefault(x => x.FullName.Equals(assemblyFullName));
 
-                if (assembly != null) {
+                if (assembly != null)
+                {
                     assembly.GetManifestResourceNames();
 
                     using var templateStream = assembly.GetManifestResourceStream(templateName);
