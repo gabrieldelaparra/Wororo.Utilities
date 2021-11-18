ad0226df2ad1553a1cdca54dd225b1880c070045
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2021-11-18 12:18:45 +0100

    SliceBy now implemented via MoreEnumerable.Partition;

--- Wororo.Utilities/EnumerableExtensions.cs
+++ Wororo.Utilities/EnumerableExtensions.cs
@@ -8,29 +8,36 @@ namespace Wororo.Utilities
 {
     public static class EnumerableExtensions
     {
-        public static void AddIfNotAlready<T>(this IList<T> list, T value) {
+        public static void AddIfNotAlready<T>(this IList<T> list, T value)
+        {
             if (!list.Contains(value))
                 list.Add(value);
         }
 
-        public static void AddSafe<T>(this IList<T> list, T value) {
+        public static void AddSafe<T>(this IList<T> list, T value)
+        {
             if (!list.Contains(value)) list.Add(value);
         }
 
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
-                Func<TSource, TKey> keySelector) {
+                Func<TSource, TKey> keySelector)
+        {
+            //Via MoreEnumerable.DistinctBy
             return MoreEnumerable.DistinctBy(source, keySelector);
         }
 
-        public static bool HasAtLeast<T>(this IEnumerable<T> enumerable, int atLeast) {
+        public static bool HasAtLeast<T>(this IEnumerable<T> enumerable, int atLeast)
+        {
             return enumerable.Take(atLeast).Count().Equals(atLeast);
         }
 
-        public static bool HasMoreThanOne<T>(this IEnumerable<T> enumerable) {
+        public static bool HasMoreThanOne<T>(this IEnumerable<T> enumerable)
+        {
             return enumerable.HasAtLeast(2);
         }
 
-        public static IEnumerable<T> IntersectIfAny<T>(this IEnumerable<T> source, IEnumerable<T> target) {
+        public static IEnumerable<T> IntersectIfAny<T>(this IEnumerable<T> source, IEnumerable<T> target)
+        {
             if (source.Any() && target.Any())
                 return source.Intersect(target);
             if (source.Any())
@@ -38,46 +45,52 @@ namespace Wororo.Utilities
             return target.Distinct();
         }
 
-        public static bool IsSingle<T>(this IEnumerable<T> enumerable) {
+        public static bool IsSingle<T>(this IEnumerable<T> enumerable)
+        {
             return enumerable.Take(2).Count().Equals(1);
         }
 
-        public static string Join(this IEnumerable<string> values, string separator) {
+        public static string Join(this IEnumerable<string> values, string separator)
+        {
             return string.Join(separator, values);
         }
 
-        public static IOrderedEnumerable<string> NaturalSort(this IEnumerable<string> list) {
+        public static IOrderedEnumerable<string> NaturalSort(this IEnumerable<string> list)
+        {
             return list.OrderBy(x => x, StringComparer.OrdinalIgnoreCase.WithNaturalSort());
         }
 
-        public static IOrderedEnumerable<T> NaturalSort<T>(this IEnumerable<T> list, Func<T, string> keySelector) {
-            return list.OrderBy(x => keySelector(x), StringComparer.OrdinalIgnoreCase.WithNaturalSort());
+        public static IOrderedEnumerable<T> NaturalSort<T>(this IEnumerable<T> list, Func<T, string> keySelector)
+        {
+            return list.OrderBy(keySelector, StringComparer.OrdinalIgnoreCase.WithNaturalSort());
         }
 
-        public static (IList<T> TrueSlice, IList<T> FalseSlice) SliceBy<T>(this IEnumerable<T> enumerable,
-                Predicate<T> predicate) {
-            var trueSlice = new List<T>();
-            var falseSlice = new List<T>();
-
-            foreach (var t in enumerable)
-                if (predicate(t))
-                    trueSlice.Add(t);
-                else
-                    falseSlice.Add(t);
-
-            return (trueSlice, falseSlice);
+        public static (IEnumerable<T> TrueSlice, IEnumerable<T> FalseSlice) SliceBy<T>(this IEnumerable<T> enumerable, Predicate<T> predicate)
+        {
+            //Via MoreEnumerable.Partition
+            return enumerable.Partition(x => predicate(x));
         }
 
-        public static IEnumerable<T> TakeRandom<T>(this IEnumerable<T> source, int takeCount) {
+        public static (IEnumerable<T> FirstPart, IEnumerable<T> SecondPart) Split<T>(this IEnumerable<T> enumerable, int fromZeroTo)
+        {
+            var first = enumerable.Take(fromZeroTo);
+            var skip = enumerable.Skip(fromZeroTo);
+            return (first, skip);
+        }
+
+        public static IEnumerable<T> TakeRandom<T>(this IEnumerable<T> source, int takeCount)
+        {
             var r = new Random();
             return source.OrderBy(x => r.NextDouble()).Take(takeCount);
         }
 
-        public static string ToSpacedCSV(this IEnumerable<string> values) {
+        public static string ToSpacedCSV(this IEnumerable<string> values)
+        {
             return values.Join(", ");
         }
 
-        public static string ToTSV(this IEnumerable<string> values) {
+        public static string ToTSV(this IEnumerable<string> values)
+        {
             return values.Join("\t");
         }
     }
