04170ceb05888cf1e4751f9e663bfa0cf1c9c48a
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2023-01-16 09:10:52 +0100

    Add basic unit test for the XML serialization.

---
+++ Wororo.Utilities.UnitTests/XmlSerializationUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Wororo.Utilities.UnitTests
+{
+  public class XmlSerializationUnitTests
+  {
+  
+    [Test]
+    public void SerializeXml_Deserialize_ValidInput()
+    {
+      //Arrange
+      var person = new MockPerson { Name = "John", Age = 30 };
+      //Act
+      person.SerializeXml("SerializeXml_ValidInput_ShouldReturnXmlString.xml");
+      var result = XmlSerialization.DeserializeXml<MockPerson>("SerializeXml_ValidInput_ShouldReturnXmlString.xml");
+      //Assert
+      Assert.IsNotNull(result);
+      Assert.AreEqual("John", result.Name);
+      Assert.AreEqual(30, result.Age);
+    }
+
+    public class MockPerson
+    {
+      public string Name { get; set; }
+      public int Age { get; set; }
+    }
+  }
+ 
+}
